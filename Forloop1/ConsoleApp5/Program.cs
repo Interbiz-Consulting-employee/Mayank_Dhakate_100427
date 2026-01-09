336447124b6095cb51a1bab2e03256a2bd55ed59
Mohan Patel <mpatel@invessio.com>
2026-01-09 09:58:36 +0530

    Add generated NuGet props and targets files for ConsoleApp7
    - Created ConsoleApp7.csproj.nuget.g.props and ConsoleApp7.csproj.nuget.g.targets to manage NuGet package references.
    - Added .NET 8.0 assembly attributes and assembly info files for the project.
    - Generated necessary cache and assets files for project dependencies.
    - Included global usings and editor config for .NET 8.0 compatibility.

---
+++ Forloop1/ConsoleApp5/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int i = 0;
+            for (; i <= 6;  i++  )
+            {
+                Console.WriteLine(i);
+            }
+
+            
+        }
+    }
+}
---
+++ Static1/ConsoleApp7/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp7
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p1 = new Program();
+        }
+    }
+}
