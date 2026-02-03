a56a5e644932e40b12e40cf880c62488f4399ba9
Mohan Patel <mpatel@invessio.com>
2026-02-03 10:03:13 +0530

    Add generated NuGet files for project assets and targets
    - Created Task_Linq.csproj.nuget.g.props to manage NuGet package properties.
    - Added Task_Linq.csproj.nuget.g.targets for build targets related to NuGet.
    - Generated project.assets.json to define project dependencies and assets.
    - Included project.nuget.cache to cache NuGet package information.

---
+++ Task_Linq/Task_Linq/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task_Linq
+{
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> list = new List<string>();
+            //var result = list.FirstOrDefault();
+            //Console.WriteLine(result);
+            ////var result1=list.First();
+            ////Console.WriteLine(result1);
+
+            List<string> employees = new List<string>
+        {
+            "Amit","Ravi","Neha","Pooja","Rahul",
+            "Kiran","Sonal","Vijay","Ankit","Priya",
+            "Deepak","Rohit","Sneha","Manoj","Kajal"
+        };
+
+            int pageNumber = 3;   // Which page you want
+            int pageSize = 5;     // Records per page
+
+            var result2 = employees
+                         .Skip((pageNumber - 1) * pageSize)
+                         .Take(pageSize);
+
+            foreach (var emp in result2)
+            {
+                Console.WriteLine(emp);
+            }
+        }
+    }
+}
