216157af609e378abbd2f1e8461641b146d8fe02
Mohan Patel <mpatel@invessio.com>
2026-02-10 18:54:00 +0530

    Add NuGet configuration and project assets for Task_69
    - Created Task_69.csproj.nuget.g.props to define NuGet properties.
    - Added Task_69.csproj.nuget.g.targets for build targets.
    - Generated project.assets.json to manage project dependencies and assets.
    - Included project.nuget.cache for caching NuGet package information.

---
+++ Task_69/Task_69/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task_69
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = { "flower", "flow", "flight" };
+            //string[] strs = { "dog", "racecar", "car" };
+
+            string result = LongestCommonPrefix(strs);
+            Console.WriteLine(result);   
+        }
+
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0)
+                return "";
+
+            Array.Sort(strs);
+
+            string first = strs[0];
+            string last = strs[strs.Length - 1];
+
+            int i = 0;
+            while (i < first.Length && i < last.Length && first[i] == last[i])
+            {
+                i++;
+            }
+
+            return first.Substring(0, i);
+        }
+    }
+}
