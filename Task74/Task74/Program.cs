51693b3fe59a5ea3a185e13c8b7d183695ba3692
Mohan Patel <mpatel@invessio.com>
2026-02-28 09:48:44 +0530

    Added Task 74 solution

---
+++ Task74/Task74/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task74
+{
+    internal class Program
+    {
+        public static long FindMissingDivisor(long n, int[] arr)
+        {
+            long totalSum = 0;
+
+            // Step 1: Find sum of all divisors of n
+            for (long i = 1; i * i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    long pair = n / i;
+
+                    totalSum += i;
+
+                    if (i != pair)
+                        totalSum += pair;
+                }
+            }
+
+            // Step 2: Sum of given array
+            long arrSum = 0;
+            foreach (int num in arr)
+                arrSum += num;
+
+            // Step 3: Missing divisor
+            return totalSum - arrSum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindMissingDivisor(100, new int[] { 1, 2, 4, 5, 10, 20, 50, 100 })); 
+            Console.WriteLine(FindMissingDivisor(36, new int[] { 2, 3, 4, 6, 9, 12, 18, 36 }));
+            
+
+        }
+    }
+}
