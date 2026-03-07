6cd96446d1f1a6de1e5e6c265c32bbaa8b291315
Mohan Patel <mpatel@invessio.com>
2026-03-07 10:56:33 +0530

    Task 76

---
+++ Task_76/Task_76/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_76
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPowerOfTwo(1));   // true
+            Console.WriteLine(IsPowerOfTwo(16));  // true
+            Console.WriteLine(IsPowerOfTwo(3));   // false
+            Console.WriteLine(IsPowerOfTwo(-8));  // false
+            Console.WriteLine(IsPowerOfTwo(0));   // false
+            Console.WriteLine(IsPowerOfTwo(230)); //false
+        }
+        static bool IsPowerOfTwo(int n)
+        {
+            
+            if (n <= 0)
+                return false;
+
+            // Loop method: keep dividing by 2
+            while (n % 2 == 0)
+            {
+                n /= 2;  
+            }
+
+            // If we end with 1, it's a power of two
+            return n == 1;
+        }
+    }
+}
