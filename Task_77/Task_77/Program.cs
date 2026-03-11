782a37672da00f47064d419c77da6128e0045366
Mohan Patel <mpatel@invessio.com>
2026-03-11 19:47:28 +0530

    Add Task 77 Nim Game solution

---
+++ Task_77/Task_77/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task_77
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input
+            Console.Write("Enter number of stones: ");
+            int n = int.Parse(Console.ReadLine());
+
+            bool canWin = false;
+
+            // Loop to check possible moves
+            for (int i = 1; i <= 3; i++)
+            {
+                if (n - i == 0)
+                {
+                    canWin = true;
+                    break;
+                }
+                else if ((n - i) % 4 == 0)
+                {
+                    canWin = true;
+                    break;
+                }
+            }
+
+            // Output result
+            if (canWin)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
